d769ad0c35968cc724865fa69084aeb2832165a8
farihamehereen15 <fariha15-12875@diu.edu.bd>
2022-11-07 02:09:12 +0600

    genius car authentication start

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEGRIKJC_yjJPLYBATMOI5f5JNIoLpW4c",
+  authDomain: "genius-car-module-66-64835.firebaseapp.com",
+  projectId: "genius-car-module-66-64835",
+  storageBucket: "genius-car-module-66-64835.appspot.com",
+  messagingSenderId: "841292431114",
+  appId: "1:841292431114:web:42cb8ac4589fddd7a404f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
